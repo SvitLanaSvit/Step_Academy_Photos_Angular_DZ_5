68495680fb96faa1d8804d57409c2400431f0565
Spil_jf00@student.itstep.org <Spil_jf00@student.itstep.org>
2023-03-31 00:31:26 +0200

    added code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDoProbeAngular';
+  quote = '«Patience is a key element of success», Bill Gates';
+  quotesGates: string[] = [
+    'https://en.wikipedia.org/wiki/Bill_Gates',
+    'https://www.gatesfoundation.org/',
+    'https://www.gatesnotes.com/',
+    'https://twitter.com/billgates'
+  ]
 }
